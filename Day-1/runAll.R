be0b7a54e163d7796254daea61a46153de9e277b
Zhian N. Kamvar <zkamvar@gmail.com>
2018-10-02 10:50:19 +0100

    add runAll script

---
+++ Day-1/runAll.R
@@ -0,0 +1,4 @@
+rmarkdown::render(here::here("reports", "01-intro-clean.Rmd"))
+rmarkdown::render(here::here("reports", "02-descriptive-stats.Rmd"))
+rmarkdown::render(here::here("reports", "03-univariate-analysis.Rmd"))
+rmarkdown::render(here::here("reports", "04-stratified-analysis.Rmd"))
